b09dff56165a63199803adf5039764991258bd1d
Rico Rojas <djizco@yahoo.com>
2022-10-03 00:05:26 -0500

    Add eslint wepback plugin, and fix lint errors

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,3 +1,9 @@
-const config = require('./webpack.config.js');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const config = require('./webpack.config');
+
+const ESLintPluginConfig = new ESLintPlugin();
+
+config.plugins.push(ESLintPluginConfig);
 
 module.exports = config;
